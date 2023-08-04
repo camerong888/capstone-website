c3e8a12a7b3b8da3c073981592fdd96fbd03c5ba
Cameron Gordon <camerong888@gmail.com>
2023-08-04 00:40:41 -0400

    Initial UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { ChakraProvider, Box, Text, Button, Flex, Spacer } from "@chakra-ui/react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChakraProvider>
+      <Box bg="blue.500" w="100%" p={4} color="white">
+        <Flex>
+          <Box p="2">
+            <Text fontSize="xl">SummitSignal</Text>
+          </Box>
+          <Spacer />
+          <Box>
+            <Button colorScheme="teal" mr="4">
+              Create Account
+            </Button>
+            <Button colorScheme="teal">
+              Login
+            </Button>
+          </Box>
+        </Flex>
+      </Box>
+    </ChakraProvider>
   );
 }
 
